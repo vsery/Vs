47c73ace74dbd561ae51c6795064d714868d038b
Vsery <3132629604@qq.com>
2018-11-29 20:32:25 +0800

    更新配置文件, 添加路由扁平化

--- src/unit/env.js
+++ src/unit/env.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 
 Vue.prototype.VsSys = {
-  title: '千恩优选',
-  logo: '/static/img/logo.png',
-  adInfo: '购物，想象不到的惊喜！', //广告标语
-  defaultHeadImg: '/static/img/header.png', // 用户的默认头像
-  serviceTel: '400-833-1519', // 客服电话
-  serviceImg: '/static/img/serviceCode.jpg', // 客服微信二维码 
-  vipcn: '/static/img/code.jpg', // 公众号二维码
-  companyName: '长沙鲁斯爱析网络科技有限公司', //公司名称
-  downloadLogo: '/static/img/dl-logo.png' //下载介绍页的logo
+    title: '千恩优选',
+    logo: '/static/img/logo.png',
+    adInfo: '购物，想象不到的惊喜！', //广告标语
+    defaultHeadImg: '/static/img/header.png', // 用户的默认头像
+    serviceTel: '400-833-1519', // 客服电话
+    serviceImg: '/static/img/serviceCode.jpg', // 客服微信二维码 
+    vipcn: '/static/img/code.jpg', // 公众号二维码
+    companyName: '长沙鲁斯爱析网络科技有限公司', //公司名称
+    downloadLogo: '/static/img/dl-logo.png' //下载介绍页的logo
 }
 
 Vue.prototype.BaseUrl = "http://www.vs.com/"
---
+++ src/unit/ver.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+
+// 手机号
+Vue.prototype.VerPhone = function(res) {
+    const phone = /^0?1[3|4|5|6|7|8|9][0-9]\d{8}$/;
+    if (!phone.test(res)) {
+        return false;
+    }
+    return true;
+}
+
+// 数字
+Vue.prototype.VerNumber = function(res) {
+    const number = /^[0-9]*$/;
+    if (!number.test(res)) {
+        return false
+    }
+    return true
+}
+
+// 电子邮箱
+Vue.prototype.VerEmail = function(res) {
+    const email = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+    if (email.test(res)) {
+        return false
+    }
+    return true
+}
+
+// 中文
+Vue.prototype.VerChinese = function(res) {
+    const chinese = /[^ ^\u4e00-\u9fa5]+$/;
+    if (!chinese.test(res)) {
+        return false
+    }
+    return true
+}
