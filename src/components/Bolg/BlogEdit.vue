ed0dcd83c4a0879a667d1ad0cd7bbd3d481e118a
Vsery <3132629604@qq.com>
2018-11-29 10:54:41 +0800

    add sys file

---
+++ src/components/Bolg/BlogEdit.vue
@@ -0,0 +1,29 @@
+<!--  -->
+<template>
+    <div class='BlogEdit'></div>
+</template>
+<script>
+export default {
+    name: 'BlogEdit',
+    data() {
+        return {}
+    },
+    beforeRouteEnter(to, from, next) {
+        next();
+        // console.group('在渲染该组件的对应路由被 confirm 前调用');
+    },
+    created() {},
+    mounted() {
+        this.$nextTick(function() {});
+    },
+    methods: {},
+    beforeDestroy() {
+        // console.group('销毁前状态  ===============>> beforeDestroy');
+    },
+    beforeRouteLeave(to, from, next) {
+        next();
+        // console.group('离开该组件的对应路由时调用');
+    },
+}
+</script>
+<style scoped></style>
