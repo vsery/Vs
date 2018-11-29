242e6b63fe09cffd29a124f06ca4d283626dddcb
Vsery <3132629604@qq.com>
2018-11-29 21:08:50 +0800

    import pulic.scss

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+    name: 'App'
 }
-</script>
 
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}*/
+
+</style>
+<style lang="scss">
+@import '~@/assets/css/public-class.scss';
+
 </style>
