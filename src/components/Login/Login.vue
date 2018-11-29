579b681cf605f1ae61bb0e81835f35a2b47014eb
Vsery <3132629604@qq.com>
2018-11-29 16:47:29 +0800

    add Layout

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,6 +1,43 @@
 <!--  -->
 <template>
-    <div class='Login'></div>
+    <div class='Login'>
+        <div flex="dir:top main:center cross:center" class="page-login--content-main"><img src="/img/logo@2x.05fe4930.png" class="page-login--logo">
+            <div class="page-login--form">
+                <div class="el-card is-never-shadow">
+                    <div class="el-card__body">
+                        <form class="el-form el-form--label-top">
+                            <div class="el-form-item is-required el-form-item--default">
+                                <div class="el-form-item__content">
+                                    <div class="el-input el-input--default el-input-group el-input-group--prepend">
+                                        <div class="el-input-group__prepend"><i class="fa fa-user-circle-o"></i></div><input type="text" autocomplete="off" placeholder="用户名" class="el-input__inner">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="el-form-item is-required el-form-item--default">
+                                <div class="el-form-item__content">
+                                    <div class="el-input el-input--default el-input-group el-input-group--prepend">
+                                        <div class="el-input-group__prepend"><i class="fa fa-keyboard-o"></i></div><input type="password" autocomplete="off" placeholder="密码" class="el-input__inner">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="el-form-item is-required el-form-item--default">
+                                <div class="el-form-item__content">
+                                    <div class="el-input el-input--default el-input-group el-input-group--append el-input-group--prepend">
+                                        <div class="el-input-group__prepend">验证码</div><input type="text" autocomplete="off" placeholder="- - - -" class="el-input__inner">
+                                        <div class="el-input-group__append"><img src="/img/login-code.10fef840.png" class="login-code"></div>
+                                    </div>
+                                </div>
+                            </div><button type="button" class="el-button button-login el-button--primary el-button--default">
+                        </form>
+                    </div>
+                </div>
+                <p flex="main:justify cross:center" class="page-login--options"><span><i aria-hidden="true" class="fa fa-question-circle"></i> 忘记密码</span><span>注册用户</span></p><button type="button" class="el-button page-login--quick el-button--info el-button--default">
+                    <span>
+            快速选择用户（测试功能）
+          </span></button>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -25,5 +62,6 @@ export default {
         // console.group('离开该组件的对应路由时调用');
     },
 }
+
 </script>
 <style scoped></style>
---
+++ src/components/README.md
@@ -0,0 +1,29 @@
+# components
+
+> 存放页面, 工具等..
+
+## directory
+
+Admin       // 管理员
+
+Bolg        // 博客目录[文章]
+
+Books       // 书籍目录[课程|小说|书籍]
+
+Calendar    // 日历目录[行程|日记|感悟]
+
+Layout      // 布局[主体布局]
+
+Login       // 登录
+
+Music       // 音乐管理
+
+Order       // 订单管理
+
+Sys         // 系统管理
+
+Tool        // 工具管理
+
+User        // 用户管理[粉丝]
+
+Video       // 视频管理
