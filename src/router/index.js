3da324418ac4a09b9d42df73f1f49fd6caa42577
Vsery <3132629604@qq.com>
2018-11-29 20:36:07 +0800

    updata router

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,38 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import VueDoc from '@/components/VueDoc'
 
+// 进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 路由数据
+import routes from './modules/routerConfig'
+
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  routes: [{
-    path: '/',
-    name: 'VueDoc',
-    component: VueDoc
-  }]
+// export default new Router({
+const router = new Router({
+    mode: 'history',
+    routes
+    // routes: [{
+    //     path: '/',
+    //     name: 'VueDoc',
+    //     component: VueDoc
+    // }]
 })
+
+/**
+ * 路由拦截
+ * 权限验证
+ */
+router.beforeEach((to, from, next) => {
+    NProgress.start()
+    // console.log('beforeEach', to, from);
+    next()
+})
+
+router.afterEach((to, from) => {
+    NProgress.done()
+    // console.log('afterEach', to, from);
+})
+export default router
---
+++ src/router/modules/routerConfig.js
@@ -0,0 +1,58 @@
+import util from '@/unit/util'
+
+import Index from '@/components/Index'
+import Login from '@/components/Login/Login'
+
+import NotFound from '@/components/Error/NotFound'
+
+import Layout from '@/components/Layout/Layout'
+
+
+const routerConfig = [
+    // 首页 必须 name:index
+    {
+        path: '/',
+        name: 'index',
+        layout: Layout,
+        component: Index
+    }
+]
+
+const routerConfigMenuOut = [
+    // 登录
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            requiresAuth: false
+        }
+    },
+    // 404
+    {
+        path: '*',
+        component: NotFound
+    }
+]
+
+// 导出全部路由设置
+// 这个数据会在 router.js 中被扁平处理
+
+export default util.recursiveRouterConfig([
+    ...routerConfig,
+    ...routerConfigMenuOut
+])
+
+// 导出参与多标签页处理的路由设置
+// 这个数据会在 mian.js 中使用
+
+export const frameInRoutes = util.recursiveRouterConfig(routerConfig).map(e => {
+    const route = e.children ? e.children[0] : e
+    return {
+        path: e.path,
+        name: route.name,
+        hidden: route.hidden,
+        meta: route.meta
+    }
+})
+console.log(frameInRoutes);
