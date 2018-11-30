051f3de984022d734f9d663fd984fba0f9dde548
Vsery <3132629604@qq.com>
2018-11-30 17:47:12 +0800

    add import modules

---
+++ src/modules.js
@@ -0,0 +1,17 @@
+// Element UI
+// https://element.faas.ele.me/#/zh-CN/component/installation
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// flex 布局库
+// https://github.com/lzxb/flex.css/
+import 'flex.css'
+
+// 图标字体
+// http://fontawesome.dashgame.com/#new
+import '@/assets/font/css/font-awesome.css'
+
+// 动画
+// https://daneden.github.io/animate.css/
+import '@/assets/css/animate.css'
