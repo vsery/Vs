d54dff4c5da3ac0cc043c19348eb25eeca0b93c3
Vsery <3132629604@qq.com>
2018-11-30 17:48:54 +0800

    update Layout

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,31 +1,118 @@
 <!--  -->
 <template>
-    <div class='Layout'>
-        
+    <!-- <div class="d2-layout-header-aside-group" :style="styleLayoutMainGroup" :class="{grayMode: grayActive}"> -->
+    <div class="d2-layout-header-aside-group">
+        <!-- 半透明遮罩 -->
+        <div class="d2-layout-header-aside-mask"></div>
+        <!-- 主体内容 -->
+        <div class="d2-layout-header-aside-content" flex="dir:top">
+            <!-- 顶栏 -->
+            <!-- <div class="d2-theme-header" :style="{opacity: this.searchActive ? 0.5 : 1 }" flex-box="0" flex> -->
+            <div class="d2-theme-header" flex-box="0" flex>
+                <!-- <div class="logo-group" :style="{width: asideCollapse ? asideWidthCollapse : asideWidth}" flex-box="0">
+                    <img v-if="asideCollapse" :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/icon-only.png`">
+                    <img v-else :src="`${$baseUrl}image/theme/${themeActiveSetting.name}/logo/all.png`">
+                </div> -->
+                <div class="toggle-aside-btn" @click="handleToggleAside" flex-box="0">
+                    <!-- <vs-icon name="bars" /> -->
+                </div>
+                <vs-menu-header flex-box="1" />
+                <!-- 顶栏右侧 -->
+                <div class="d2-header-right" flex-box="0">
+                    <!-- 如果你只想在开发环境显示这个按钮请添加 v-if="$env === 'development'" -->
+                    <!-- <vs-header-search @click="handleSearchClick" />
+                    <vs-header-error-log />
+                    <vs-header-fullscreen />
+                    <vs-header-theme />
+                    <vs-header-size />
+                    <vs-header-user /> -->
+                </div>
+            </div>
+            <!-- 下面 主体 -->
+            <div class="d2-theme-container" flex-box="1" flex>
+                <!-- 主体 侧边栏 -->
+                <!-- <div flex-box="0" ref="aside" class="d2-theme-container-aside" :style="{width: asideCollapse ? asideWidthCollapse : asideWidth, opacity: this.searchActive ? 0.5 : 1 }"> -->
+                <div flex-box="0" ref="aside" class="d2-theme-container-aside">
+                    <!-- <vs-menu-side /> -->
+                </div>
+                <!-- 主体 -->
+                <div class="d2-theme-container-main" flex-box="1" flex>
+                    <!-- 搜索 -->
+                    <transition name="fade-scale">
+                        <!-- <div v-show="searchActive" class="d2-theme-container-main-layer" flex="dir:top">
+                            <vs-panel-search ref="panelSearch" @close="searchPanelClose" />
+                        </div> -->
+                    </transition>
+                    <!-- 内容 -->
+                    <transition name="fade-scale">
+                        <!-- <div v-show="!searchActive" class="d2-theme-container-main-layer" flex="dir:top"> -->
+                        <div class="d2-theme-container-main-layer" flex="dir:top">
+                            <!-- tab -->
+                            <!-- <div class="d2-theme-container-main-header" flex-box="0">
+                                <vs-tabs />
+                            </div> -->
+                            <!-- 页面 -->
+                            <!-- <div class="d2-theme-container-main-body" flex-box="1">
+                                <transition :name="transitionActive ? 'fade-transverse' : ''">
+                                    <keep-alive :include="keepAlive">
+                                        <router-view />
+                                    </keep-alive>
+                                </transition>
+                            </div> -->
+                        </div>
+                    </transition>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex'
 export default {
     name: 'Layout',
     data() {
         return {}
     },
-    beforeRouteEnter(to, from, next) {
-        next();
-        // console.group('在渲染该组件的对应路由被 confirm 前调用');
+    components: {
+        // 'vs-menu-side': () => import('./modules/menu-side'),
+        'vs-menu-header': () => import('./modules/menu-header'),
+        // 'vs-tabs': () => import('./modules/tabs'),
+        // 'vs-header-fullscreen': () => import('./modules/header-fullscreen'),
+        // 'vs-header-search': () => import('./modules/header-search'),
+        // 'vs-header-size': () => import('./modules/header-size'),
+        // 'vs-header-theme': () => import('./modules/header-theme'),
+        // 'vs-header-user': () => import('./modules/header-user'),
+        // 'vs-header-error-log': () => import('./modules/header-error-log')
+    },
+    computed: {
+        // ...mapState('d2admin', {
+        //     keepAlive: state => state.page.keepAlive,
+        //     grayActive: state => state.gray.active,
+        //     transitionActive: state => state.transition.active,
+        //     asideCollapse: state => state.menu.asideCollapse
+        // }),
+        // ...mapGetters('d2admin', {
+        //     themeActiveSetting: 'theme/activeSetting'
+        // }),
+        /**
+         * @description 最外层容器的背景图片样式
+         */
+        // styleLayoutMainGroup() {
+        //     return {
+        //         ...this.themeActiveSetting.backgroundImage ? {
+        //             backgroundImage: `url('${this.$baseUrl}${this.themeActiveSetting.backgroundImage}')`
+        //         } : {}
+        //     }
+        // }
     },
     created() {},
     mounted() {
         this.$nextTick(function() {});
     },
-    methods: {},
-    beforeDestroy() {
-        // console.group('销毁前状态  ===============>> beforeDestroy');
-    },
-    beforeRouteLeave(to, from, next) {
-        next();
-        // console.group('离开该组件的对应路由时调用');
+    methods: {
+        handleToggleAside() {}
     },
 }
+
 </script>
 <style scoped></style>
---
+++ src/components/Layout/modules/menu-header.vue
@@ -0,0 +1,50 @@
+<!--  -->
+<template>
+    <div class='menu-header'>
+        menu-header
+        <!-- <div class="d2-theme-header-menu" ref="page" :class="{'is-scrollable': isScroll}" flex="cross:center">
+            <div class="d2-theme-header-menu__content" ref="content" flex-box="1" flex>
+                <div class="d2-theme-header-menu__scroll" ref="scroll" flex-box="0" :style="'transform: translateX(' + currentTranslateX + 'px);'">
+                    <el-menu mode="horizontal" :default-active="active" @select="handleMenuSelect">
+                        <template v-for="(menu, menuIndex) in header">
+                            <d2-layout-header-aside-menu-item v-if="menu.children === undefined" :menu="menu" :key="menuIndex" />
+                            <d2-layout-header-aside-menu-sub v-else :menu="menu" :key="menuIndex" />
+                        </template>
+                    </el-menu>
+                </div>
+            </div>
+            <div v-if="isScroll" class="d2-theme-header-menu__prev" flex-box="0" @click="scroll('left')" flex="main:center cross:center">
+                <i class="el-icon-arrow-left"></i>
+            </div>
+            <div v-if="isScroll" class="d2-theme-header-menu__next" flex-box="0" @click="scroll('right')" flex="cross:center">
+                <i class="el-icon-arrow-right"></i>
+            </div>
+        </div> -->
+    </div>
+</template>
+<script>
+export default {
+    name: 'menu-header',
+    data() {
+        return {}
+    },
+    beforeRouteEnter(to, from, next) {
+        next();
+        // console.group('在渲染该组件的对应路由被 confirm 前调用');
+    },
+    created() {},
+    mounted() {
+        this.$nextTick(function() {});
+    },
+    methods: {},
+    beforeDestroy() {
+        // console.group('销毁前状态  ===============>> beforeDestroy');
+    },
+    beforeRouteLeave(to, from, next) {
+        next();
+        // console.group('离开该组件的对应路由时调用');
+    },
+}
+
+</script>
+<style scoped></style>
